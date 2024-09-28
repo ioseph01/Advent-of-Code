e3a980ecf727d23378b4b37c66a2ef411aabc40e
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2024-09-27 20:24:28 -0700

    Add files via upload

---
+++ 2023/2.py
@@ -0,0 +1,25 @@
+
+table = [line.split(" ") for line in open("f.txt").read().split("\n")]
+
+result = 0
+
+for x in table:
+    differences = [x]
+    while 1 != len(set(differences[-1])):
+        differences.append([int(differences[-1][n + 1]) - int(differences[-1][n]) for n in range(len(differences[-1]) - 1)])
+
+
+
+    back = differences[-1][-1]
+    
+    for l in reversed(range(len(differences) - 1)):
+        
+        back = int(differences[l][0]) - back
+
+
+    result += back
+
+
+
+
+print(result)
---
+++ 2023/Aoc_9.py
@@ -0,0 +1,22 @@
+
+table = [line.split(" ") for line in open("f.txt").read().split("\n")]
+
+result = 0
+
+for x in table:
+    differences = [x]
+    while 1 != len(set(differences[-1])):
+        differences.append([int(differences[-1][n + 1]) - int(differences[-1][n]) for n in range(len(differences[-1]) - 1)])
+
+
+    difference = differences[-1][-1]
+    for l in reversed(range(len(differences) - 1)):
+        difference = int(differences[l][-1]) + difference
+    
+
+    result += difference
+
+
+
+
+print(result)
