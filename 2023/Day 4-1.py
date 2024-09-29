4f2de9736fac0b27d2b4a795651f54f9a67a0a68
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2024-09-29 11:18:30 -0700

    Update Day 4-1.py
    :=

--- 2023/Day 4-1.py
+++ 2023/Day 4-1.py
@@ -5,8 +5,8 @@ result = 0
 with open("f.txt") as f:
     for line in f:
         line = [set([num for num in card.split()]) for card in line.strip().split(": ")[-1].split(" | ")]
-        matches = len(line[0] & line[-1])
-        if matches > 0:
+        
+        if (matches := len(line[0] & line[-1])) > 0:
             result += 2 ** (matches - 1)
         
 print(result)
